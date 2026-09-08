ba4f672499e24507ce60bc41148cb38c07b31cf5
Luis Alvarado <5511505+rhynocerus@users.noreply.github.com>
2026-09-08 21:04:59 +0200

    chore: publish web build with extended music

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788869057|3930070';
+const CACHE_VERSION = '1788894224|4548282';
 /** @type {string} */
 const CACHE_PREFIX = 'Packet Runner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
