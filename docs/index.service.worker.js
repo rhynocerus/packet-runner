76b20a2979c9f07b77cfe8a4ea8f95440d92223a
Luis Alvarado <5511505+rhynocerus@users.noreply.github.com>
2026-09-09 14:59:41 +0200

    build: publish optimized web version

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788894224|4548282';
+const CACHE_VERSION = '1788958098|2308982';
 /** @type {string} */
 const CACHE_PREFIX = 'Packet Runner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
