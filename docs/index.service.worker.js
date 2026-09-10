0f89270ebc1e1b89dac5360889b7a9f617ce301b
Luis Alvarado <5511505+rhynocerus@users.noreply.github.com>
2026-09-10 15:15:59 +0200

    build: publish smooth level transitions

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789044084|2301057';
+const CACHE_VERSION = '1789046100|2265672';
 /** @type {string} */
 const CACHE_PREFIX = 'Packet Runner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
