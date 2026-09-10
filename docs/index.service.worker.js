07121590979d4ebe5ff4686634a4eff709ed8e87
Luis Alvarado <5511505+rhynocerus@users.noreply.github.com>
2026-09-10 14:45:41 +0200

    build: publish cyber savanna web version

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788958098|2308982';
+const CACHE_VERSION = '1789044084|2301057';
 /** @type {string} */
 const CACHE_PREFIX = 'Packet Runner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
