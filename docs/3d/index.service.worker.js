fed2cb003fcb6f34f7e8f47300fcef1769cfa089
Luis Alvarado <5511505+rhynocerus@users.noreply.github.com>
2026-09-11 22:00:38 +0200

    build: publish Packet Runner 3D cinematic intro v0.7.1

--- docs/3d/index.service.worker.js
+++ docs/3d/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789150974|5418746';
+const CACHE_VERSION = '1789156817|2331649';
 /** @type {string} */
 const CACHE_PREFIX = 'Packet Runner-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
